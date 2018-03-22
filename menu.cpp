19d03556d10e0fe7e35f0ce51bd6658650d1a8db
Ricardo MG Silva <up201607780@fe.up.pt>
2018-03-22 18:33:45 +0000

    Test github

---
+++ menu.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+
+  std::cout << "Test file for the github" << endl;
+  return 0;
+}
